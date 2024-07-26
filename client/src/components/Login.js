4d24f821e4d04ef153cc7e0b1e7a7b58a6082216
pranav-1512 <135528405+pranav-1512@users.noreply.github.com>
2024-07-26 16:52:20 +0530

    Update Login.js

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -12,7 +12,7 @@ const Login = ({ onLogin }) => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post('http://localhost:5000/auth/login', {
+      const res = await axios.post('https://gallery-app-server-sigma.vercel.app/auth/login', {
         email: email,
         password: password,
       });
